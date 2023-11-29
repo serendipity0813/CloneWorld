e71d88499a0676e6eb13c6e2349ec04044d047d0
serendipity0813 <91657737+serendipity0813@users.noreply.github.com>
2023-11-29 17:49:09 +0900

    npc1, npc2

---
+++ Assets/Scripts/UI/ButtonLink.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttonlink : MonoBehaviour
+{
+    // Start is called before the first frame update 
+
+    public void TILButton()
+    {
+        Application.OpenURL("https://docs.google.com/forms/d/e/1FAIpQLSeCYwfcHZnG6URB7blUd0rCfMaNjyvPAqWZeQBE9he3H2XhHQ/viewform");
+    }
+    public void CodeKataButton()
+    {
+        Application.OpenURL(" https://docs.google.com/forms/d/e/1FAIpQLSduIuX-AsMhatLozQyP76qnlRhNISGXyQrRB0GxtR4xJA0A7A/viewform?usp=sf_link");
+    }
+    public void CodeKataNotionButton()
+    {
+        Application.OpenURL("https://www.notion.so/teamsparta/6f6f701d2c3643af86328355520a0540");
+    }
+
+
+
+
+
+}
--- Assets/Scripts/UI/NPCtextUI.cs
+++ Assets/Scripts/UI/NPCtextUI.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class NPCtextUI : MonoBehaviour
 {
+    public GameObject Player;
+    public GameObject NPC1;
+    public GameObject NPC2;
+    public GameObject NPC3;
+    public GameObject UI1;
+    public GameObject UI2;
+    public GameObject UI3;
+    float NPC1Distance = 0f;
+    float NPC2Distance = 0f;
+    float NPC3Distance = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        NPC1Distance = Vector2.Distance(Player.transform.position, NPC1.transform.position);
+        NPC2Distance = Vector2.Distance(Player.transform.position, NPC2.transform.position);
+        NPC3Distance = Vector2.Distance(Player.transform.position, NPC3.transform.position);
+
+        if (NPC1Distance < 5f)
+            UI1.SetActive(true);
+        else if (NPC2Distance < 5f)
+            UI2.SetActive(true);
+        else if (NPC3Distance < 5f)
+            UI3.SetActive(true);
+        else
+        {
+            UI1.SetActive(false);
+            UI2.SetActive(false);
+            UI3.SetActive(false);
+        }
     }
+
+    void NPCUI()
+    {
+
+    }
+
+
 }
