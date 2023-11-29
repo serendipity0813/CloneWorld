d6bdbbe2edcb909e453b9a26318d8c056446966a
serendipity0813 <91657737+serendipity0813@users.noreply.github.com>
2023-11-29 17:01:05 +0900

    JUMP

--- Assets/Scripts/Controllers/AnimationController.cs
+++ Assets/Scripts/Controllers/AnimationController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class AnimationController : MonoBehaviour
 {
@@ -16,6 +18,7 @@ public class AnimationController : MonoBehaviour
     public SpriteRenderer player; // 스프라이트 렌더러 선언
 
 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,7 +28,46 @@ public class AnimationController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) )
+        {
+            MoveAnimation();
+        }
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
+        {
+            AnimationStop();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            JumpAnimation();
+        }
+
+    }
+
+    void MoveAnimation()
+    {
+            Character.SetBool("Run1",true);
+            Character.SetBool("Run2", true);
+            Character.SetBool("Run3", true);
+            Character.SetBool("Run4", true);
+    }
+
+    void JumpAnimation()
+    {
+        Character.SetTrigger("Jump1");
+        Character.SetTrigger("Jump2");
+        Character.SetTrigger("Jump3");
+        Character.SetTrigger("Jump4");
+
+    }
+
+    void AnimationStop()
+    {
+            Character.SetBool("Run1", false);
+            Character.SetBool("Run2", false);
+            Character.SetBool("Run3", false);
+            Character.SetBool("Run4", false);
     }
 
     public void player1Push()
